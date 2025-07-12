6617b5265e08c20895bde98d9f6b27a8fc55256d
real2two <26689496+real2two@users.noreply.github.com>
2025-07-12 12:21:25 -0400

    fix: handles postgresql errors properly

--- src/db/adapters/psql.ts
+++ src/db/adapters/psql.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import { Pool } from "pg";
 import type { Storage } from "../types";
 
@@ -6,16 +7,28 @@ export class PostgresStorage implements Storage {
 
 	constructor({ connectionString }: { connectionString: string }) {
 		this.pool = new Pool({ connectionString });
+
+		this.pool.on("error", (err) => {
+			console.error(`${chalk.red.bold("[RUCS ERROR]")} PostgreSQL error:`, err);
+		});
 	}
 
 	async get(key: string): Promise<string | null> {
 		if (typeof key !== "string") throw new Error("key must be a string");
 
-		const res = await this.pool.query(
-			"SELECT value FROM kv_store WHERE key=$1 LIMIT 1",
-			[key],
-		);
-		return res.rows[0]?.value ?? null;
+		try {
+			const res = await this.pool.query(
+				"SELECT value FROM kv_store WHERE key=$1 LIMIT 1",
+				[key],
+			);
+			return res?.rows[0]?.value ?? null;
+		} catch (err) {
+			console.error(
+				`${chalk.red.bold("[RUCS ERROR]")} PostgreSQL error (get):`,
+				err,
+			);
+			return null;
+		}
 	}
 
 	async find(
@@ -44,8 +57,16 @@ export class PostgresStorage implements Storage {
 			sql += ` OFFSET $${values.length}`;
 		}
 
-		const res = await this.pool.query(sql, values);
-		return res.rows;
+		try {
+			const res = await this.pool.query(sql, values);
+			return res.rows;
+		} catch (err) {
+			console.error(
+				`${chalk.red.bold("[RUCS ERROR]")} PostgreSQL error (find):`,
+				err,
+			);
+			return [];
+		}
 	}
 
 	async count(pattern: string): Promise<number> {
@@ -53,27 +74,49 @@ export class PostgresStorage implements Storage {
 			throw new Error("pattern must be a string");
 		}
 
-		const res = await this.pool.query(
-			"SELECT COUNT(*) FROM kv_store WHERE key LIKE $1",
-			[pattern],
-		);
-		return Number.parseInt(res.rows[0].count, 10);
+		try {
+			const res = await this.pool.query(
+				"SELECT COUNT(*) FROM kv_store WHERE key LIKE $1",
+				[pattern],
+			);
+			return Number.parseInt(res.rows[0].count, 10);
+		} catch (err) {
+			console.error(
+				`${chalk.red.bold("[RUCS ERROR]")} PostgreSQL error (count):`,
+				err,
+			);
+			return 0;
+		}
 	}
 
 	async set(key: string, value: string): Promise<void> {
 		if (typeof key !== "string") throw new Error("key must be a string");
 		if (typeof value !== "string") throw new Error("value must be a string");
 
-		await this.pool.query(
-			"INSERT INTO kv_store (key, value) VALUES ($1, $2) ON CONFLICT (key) DO UPDATE SET value=EXCLUDED.value",
-			[key, value],
-		);
+		try {
+			await this.pool.query(
+				"INSERT INTO kv_store (key, value) VALUES ($1, $2) ON CONFLICT (key) DO UPDATE SET value=EXCLUDED.value",
+				[key, value],
+			);
+		} catch (err) {
+			console.error(
+				`${chalk.red.bold("[RUCS ERROR]")} PostgreSQL error (set):`,
+				err,
+			);
+		}
 	}
 
 	async delete(key: string): Promise<void> {
 		if (typeof key !== "string") throw new Error("key must be a string");
 
-		await this.pool.query("DELETE FROM kv_store WHERE key=$1", [key]);
+		try {
+			await this.pool.query("DELETE FROM kv_store WHERE key=$1", [key]);
+		} catch (err) {
+			console.error(
+				`${chalk.red.bold("[RUCS ERROR]")} PostgreSQL error (delete):`,
+				err,
+			);
+		}
 	}
 
 	async deleteMany(pattern: string): Promise<void> {
@@ -81,6 +124,15 @@ export class PostgresStorage implements Storage {
 			throw new Error("pattern must be a string");
 		}
 
-		await this.pool.query("DELETE FROM kv_store WHERE key LIKE $1", [pattern]);
+		try {
+			await this.pool.query("DELETE FROM kv_store WHERE key LIKE $1", [
+				pattern,
+			]);
+		} catch (err) {
+			console.error(
+				`${chalk.red.bold("[RUCS ERROR]")} PostgreSQL error (deelteMany):`,
+				err,
+			);
+		}
 	}
 }
